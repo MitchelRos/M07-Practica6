1d96700472cbb73956d4baf89b9c96f710e92148
amonge20 <amonge20@jaumebalmes.net>
2022-11-10 20:30:49 +0100

    Part 1 i 3

---
+++ sum.py
@@ -0,0 +1,9 @@
+#Agafem les variables a i b per fer la funcio sum
+def suma(a,b):
+    #Imprimin a i b
+    print(a+b)
+
+#Funcio per a que main.py funcioni
+def sum():
+    # Fiquem els numeros per a i b
+    suma(3,7)
